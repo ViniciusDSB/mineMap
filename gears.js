4175e232e0833f16cfb3abc4ef9a21486118e5a2
Baron <72515991+ViniciusDSB@users.noreply.github.com>
2021-07-12 15:52:52 -0400

    Less HTML lines!
    Before the boat had hit the water the html had 60 lines of <img>, but now all you need is some JS to render the images. And one more option was added, border. That's it.

--- gears.js
+++ gears.js
@@ -1,23 +1,49 @@
-document.body.style.background = localStorage.getItem('backColor')
-for(i = 0; i<61; i++){
-    document.getElementsByTagName('img')[i].style.margin = localStorage.getItem('borderValue')
+/*Renderizar mapa*/
+for(x=-4; x<2; x++){
+    let wraper = document.getElementById('wrap')
+
+    let columnDiv = document.createElement('div')
+    columnDiv.id = `flexColumn${x}`
+
+    for(y=1; y<11; y++){
+        let columnsImgs = document.createElement('img')
+        columnsImgs.src = `day/${x},${y}.png`
+        columnsImgs.onmouseover = () => {
+            document.getElementById('zoomIMG').src = columnsImgs.src
+        }
+        columnDiv.append(columnsImgs)
+    }
+
+    wraper.append(columnDiv)
 }
 
+
+/*Funções de estilo*/
 function lightSwitch(color){
     localStorage.setItem('backColor', color)
     document.body.style.background = color
 }
 
-function borderSwitch(value){
-    localStorage.setItem('borderValue', value)
+function marginSwitch(marginValue){
+    localStorage.setItem('marginValue', marginValue)
     for(i = 0; i<61; i++){
-        document.getElementsByTagName('img')[i].style.margin = value
+        document.getElementsByTagName('img')[i].style.margin = marginValue
     }
 }
 
+function gridSwitch(borderValue){
+    localStorage.setItem('borderValue', borderValue)
+    for(i = 0; i<61; i++){
+        document.getElementsByTagName('img')[i].style.border = borderValue
+    }
+}
 
-function zoomThis(fileName){
-    document.getElementById('zoomIMG').src = fileName
-    let coordinates = fileName.substring(4,8)
-    document.getElementById('coordinates').innerText = coordinates
+/*Sessão anterior*/
+document.body.style.background = localStorage.getItem('backColor')
+for(i = 0; i<61; i++){
+    document.getElementsByTagName('img')[i].style.margin = localStorage.getItem('marginValue')
+}
+
+for(i = 0; i<61; i++){
+    document.getElementsByTagName('img')[i].style.border = localStorage.getItem('borderValue')
 }
